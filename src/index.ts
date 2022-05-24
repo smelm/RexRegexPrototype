9f7aa098b97d9fbe8388c3c12d82d1ad4926f345
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-05-24 17:39:00 +0200

    feat: simple multiline and ignore non important tokens

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export function CountOf(count: number, value: any) {
 export function CountRangeOf(from: number, to: number, value: any) {
     return { type: "count_of", from, to, value }
 }
-CountRangeOf
+
 export function ManyOf(value: any) {
     return { type: "many_of", value }
 }
@@ -23,20 +23,23 @@ export function Maybe(value: any) {
 }
 
 const actions = {
+    single_value(_input: any, _start: any, _end: any, elements: any) {
+        return elements[0]
+    },
     any(_input: any, _start: any, _end: any, _elements: any) {
         return Any()
     },
     count_of(_input: any, _start: any, _end: any, elements: any) {
-        return CountOf(parseInt(elements[0].text), elements[4])
+        return CountOf(parseInt(elements[0].text), elements[1])
     },
     count_range_of(_input: any, _start: any, _end: any, elements: any) {
-        return CountRangeOf(parseInt(elements[0].text), parseInt(elements[4].text), elements[8])
+        return CountRangeOf(parseInt(elements[0].text), parseInt(elements[1].text), elements[2])
     },
     many_of(_input: any, _start: any, _end: any, elements: any) {
-        return ManyOf(elements[4])
+        return ManyOf(elements[0])
     },
     maybe(_input: any, _start: any, _end: any, elements: any) {
-        return Maybe(elements[2])
+        return Maybe(elements[0])
     },
 }
 
--- test/parsing.test.ts
+++ test/parsing.test.ts
@@ -1,6 +1,6 @@
 import { parse, Any, CountOf, ManyOf, Maybe, CountRangeOf } from "../dist"
 
-describe("", () => {
+describe("single line expressions", () => {
     test.each([
         ["any", Any()],
         ["5 of any", CountOf(5, Any())],
@@ -10,6 +10,17 @@ describe("", () => {
         ["maybe 5 of any", Maybe(CountOf(5, Any()))],
         ["maybe many of any", Maybe(ManyOf(Any()))],
     ])("%s", (input, expected) => {
+        console.log(parse(input))
+        expect(parse(input)).toEqual(expected)
+    })
+})
+
+describe("multi line line expressions", () => {
+    test.each([
+        ["maybe with block with one line", "maybe\nany\nend", Maybe(Any())],
+        ["many of with block with one line", "many of\nany\nend", ManyOf(Any())],
+        //["3 of with block with one line", "3 of\nany\nend", CountOf(3, Any())],
+    ])("%s", (_testName, input, expected) => {
         expect(parse(input)).toEqual(expected)
     })
 })
