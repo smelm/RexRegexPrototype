eb562d38507fbe462c4b8c620babc44af7093c3f
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-05-24 16:21:45 +0200

    feat: improve initial grammar

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,33 @@ export function CountOf(count: number, value: any) {
     return { type: "count_of", count, value }
 }
 
+export function CountRangeOf(from: number, to: number, value: any) {
+    return { type: "count_of", from, to, value }
+}
+CountRangeOf
 export function ManyOf(value: any) {
     return { type: "many_of", value }
 }
 
+export function Maybe(value: any) {
+    return { type: "maybe", value }
+}
+
 const actions = {
-    any(input: any, start: any, end: any, elements: any) {
+    any(_input: any, _start: any, _end: any, _elements: any) {
         return Any()
     },
-    count_of(input: any, start: any, end: any, elements: any) {
+    count_of(_input: any, _start: any, _end: any, elements: any) {
         return CountOf(parseInt(elements[0].text), elements[4])
     },
-    many_of(input: any, start: any, end: any, elements: any) {
-        console.log(elements)
+    count_range_of(_input: any, _start: any, _end: any, elements: any) {
+        return CountRangeOf(parseInt(elements[0].text), parseInt(elements[4].text), elements[8])
+    },
+    many_of(_input: any, _start: any, _end: any, elements: any) {
         return ManyOf(elements[4])
+    },
+    maybe(_input: any, _start: any, _end: any, elements: any) {
+        return Maybe(elements[2])
     },
 }
 
--- test/parsing.test.ts
+++ test/parsing.test.ts
@@ -1,15 +1,15 @@
-import { parse, Any, CountOf, ManyOf } from "../dist"
+import { parse, Any, CountOf, ManyOf, Maybe, CountRangeOf } from "../dist"
 
 describe("", () => {
     test.each([
         ["any", Any()],
         ["5 of any", CountOf(5, Any())],
         ["many of any", ManyOf(Any())],
+        ["maybe any", Maybe(Any())],
+        ["1 to 5 of any", CountRangeOf(1, 5, Any())],
+        ["maybe 5 of any", Maybe(CountOf(5, Any()))],
+        ["maybe many of any", Maybe(ManyOf(Any()))],
     ])("%s", (input, expected) => {
         expect(parse(input)).toEqual(expected)
     })
-
-    //"maybe any"
-    //"maybe many of any"
-    //"5 of any"
 })
