f0ddb3b61fc70da4a274568ffe66bd9b17311db0
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-05-25 15:53:55 +0200

    feat: handle multiline with whitespace

--- test/parsing.test.ts
+++ test/parsing.test.ts
@@ -1,40 +1,48 @@
+import { Expression } from "typescript"
 import { parse, any, countOf, manyOf, maybe, countRangeOf, sequence } from "../dist"
 
+function generateTestNames([input, ast]) {
+    return [ast.toString(), input, ast]
+}
+
+const SINGLE_LINE_CASES = [
+    ["any", any()],
+    ["5 of any", countOf(5, any())],
+    ["many of any", manyOf(any())],
+    ["maybe any", maybe(any())],
+    ["1 to 5 of any", countRangeOf(1, 5, any())],
+    ["maybe 5 of any", maybe(countOf(5, any()))],
+    ["maybe many of any", maybe(manyOf(any()))],
+].map(generateTestNames)
+
 describe("single line expressions", () => {
-    test.each([
-        ["any", any()],
-        ["5 of any", countOf(5, any())],
-        ["many of any", manyOf(any())],
-        ["maybe any", maybe(any())],
-        ["1 to 5 of any", countRangeOf(1, 5, any())],
-        ["maybe 5 of any", maybe(countOf(5, any()))],
-        ["maybe many of any", maybe(manyOf(any()))],
-    ])("%s", (input, expected) => {
+    test.each(SINGLE_LINE_CASES)("%s", (_testName: string, input: string, expected: Expression) => {
         expect(parse(input)).toEqual(expected)
     })
 })
 
-describe("multi line line expressions", () => {
-    test.each([
-        ["maybe with block with one line", "maybe\nany\nend", maybe(sequence([any()]))],
-        ["many of with block with one line", "many of\nany\nend", manyOf(sequence([any()]))],
-        ["count with block with one line", "3 of\nany\nend", countOf(3, sequence([any()]))],
-        [
-            "count range with block with one line",
-            "3 to 5 of\nany\nend",
-            countRangeOf(3, 5, sequence([any()])),
-        ],
-        [
-            "maybe with block with multiple line",
-            "maybe\nany\nmaybe any\nend",
-            maybe(sequence([any(), maybe(any())])),
-        ],
-        [
-            "maybe with block with multiple line",
-            "maybe\nany\nmaybe any\nmany of any\nend",
-            maybe(sequence([any(), maybe(any()), manyOf(any())])),
-        ],
-    ])("%s", (_testName, input, expected) => {
+const MULTI_LINE_CASES = [
+    ["maybe\nany\nend", maybe(sequence([any()]))],
+    ["many of\nany\nend", manyOf(sequence([any()]))],
+    ["3 of\nany\nend", countOf(3, sequence([any()]))],
+    ["3 to 5 of\nany\nend", countRangeOf(3, 5, sequence([any()]))],
+    ["maybe\nany\nmaybe any\nend", maybe(sequence([any(), maybe(any())]))],
+    [
+        "maybe\nany\nmaybe any\nmany of any\nend",
+        maybe(sequence([any(), maybe(any()), manyOf(any())])),
+    ],
+].map(generateTestNames)
+
+describe("multi line expressions", () => {
+    test.each(MULTI_LINE_CASES)("%s", (_testName: string, input: string, expected: Expression) => {
+        expect(parse(input)).toEqual(expected)
+    })
+})
+
+describe("multi line expressions with random white spaces", () => {
+    test.each(MULTI_LINE_CASES)("%s", (_testName: string, input: string, expected: Expression) => {
+        const randomWhitespace = () => " ".repeat(Math.random() * 4)
+        input = input.replace("\n", `${randomWhitespace()}\n${randomWhitespace()}`)
         expect(parse(input)).toEqual(expected)
     })
 })
