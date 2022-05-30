fe70e197a1bc62a75731d1bdfa03df4b1ca9fca5
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-05-30 15:05:52 +0200

    refactor: classes as parser combinator

--- src/parsing.ts
+++ src/parsing.ts
@@ -7,8 +7,6 @@ interface ParseResult {
     value: any
 }
 
-type Parser = (input: string) => ParseResult
-
 function ok(value: any, matched: string, remaining: string): ParseResult {
     return { isSuccess: true, value, matched, remaining }
 }
@@ -17,57 +15,58 @@ function err(input: string, message: string): ParseResult {
     return { isSuccess: false, value: message, matched: "", remaining: input }
 }
 
-const spc = repeat(alternative([str(" "), str("\t")]))
+abstract class Parser {
+    constructor(public ignored: boolean = false) {}
+    abstract parse(input: string): ParseResult
 
-function str(str: string, value?: any): Parser {
-    if (!value) {
-        value = str
+    public withValue(func: Function): Parser {
+        return new ValueTransformingParser(this, func)
+    }
+}
+
+class ValueTransformingParser extends Parser {
+    constructor(private parser: Parser, private transform: Function) {
+        super()
     }
 
-    return function (input: string): ParseResult {
-        if (input.startsWith(str)) {
-            return ok(value, str, input.slice(str.length))
+    parse(input: string): ParseResult {
+        const result = this.parser.parse(input)
+
+        if (!result.isSuccess) {
+            return result
+        }
+
+        return ok(this.transform(result.value), result.matched, result.remaining)
+    }
+}
+
+class StringParser extends Parser {
+    constructor(private str: string, private value?: any, ignored: boolean = false) {
+        super(ignored)
+
+        if (!this.value) {
+            this.value = str
+        }
+    }
+
+    parse(input: string): ParseResult {
+        if (input.startsWith(this.str)) {
+            return ok(this.value, this.str, input.slice(this.str.length))
         } else {
-            return err(input, `expected '${str}' but got '${input}'`)
+            return err(input, `expected '${this.str}' but got '${input}'`)
         }
     }
 }
 
-// TODO format value
-function sequenceWithIgnore(parsers: { parser: Parser; ignored: boolean }[]): Parser {
-    return function (input: string): ParseResult {
-        let values: any[] = []
-        let remaining = input
-        for (const { parser, ignored } of parsers) {
-            const result = parser(remaining)
-
-            if (!result.isSuccess) {
-                return result // is the error
-            }
-
-            remaining = result.remaining
-
-            if (!ignored) {
-                values.push(result.value)
-            }
-        }
-
-        return ok(values, input, remaining)
+class Alternative extends Parser {
+    constructor(private parsers: Parser[], ignored: boolean = false) {
+        super(ignored)
     }
-}
 
-function tokenSequence(...tokens: Parser[]): Parser {
-    let parsers = tokens.map(parser => ({ parser, ignored: false }))
-    parsers = intersperse(parsers, { parser: spc, ignored: true })
-
-    return sequenceWithIgnore(parsers)
-}
-
-function alternative(parsers: Parser[]): Parser {
-    return function (input: string): ParseResult {
+    parse(input: string): ParseResult {
         const errors: string[] = []
-        for (const p of parsers) {
-            const result = p(input)
+        for (const p of this.parsers) {
+            const result = p.parse(input)
 
             if (result.isSuccess) {
                 return result
@@ -79,18 +78,26 @@ function alternative(parsers: Parser[]): Parser {
     }
 }
 
-function repeat(parser: Parser, optional: boolean = false): Parser {
-    return function (input: string): ParseResult {
+class Repeat extends Parser {
+    constructor(
+        private parser: Parser,
+        private optional: boolean = false,
+        ignored: boolean = false
+    ) {
+        super(ignored)
+    }
+
+    parse(input: string): ParseResult {
         let values: any[] = []
 
-        let result = parser(input)
+        let result = this.parser.parse(input)
 
         while (result.isSuccess) {
             values.push(result.value)
-            result = parser(result.remaining)
+            result = this.parser.parse(result.remaining)
         }
 
-        if (!optional && values.length === 0) {
+        if (!this.optional && values.length === 0) {
             return err(input, `expected at least one but ran into error: ${result.value}`)
         }
 
@@ -98,15 +105,39 @@ function repeat(parser: Parser, optional: boolean = false): Parser {
     }
 }
 
-function withValue(parser: Parser, f: Function): Parser {
-    return function (input: string): ParseResult {
-        const result = parser(input)
+const spc = new Repeat(
+    new Alternative([new StringParser(" "), new StringParser("\t")]),
+    false,
+    true
+)
 
-        if (!result.isSuccess) {
-            return result
+class Sequence extends Parser {
+    constructor(private parsers: Parser[], ignored: boolean = false) {
+        super(ignored)
+    }
+
+    parse(input: string): ParseResult {
+        let values: any[] = []
+        let remaining = input
+        for (const parser of this.parsers) {
+            const result = parser.parse(remaining)
+
+            if (!result.isSuccess) {
+                return result // is the error
+            }
+
+            remaining = result.remaining
+
+            if (!parser.ignored) {
+                values.push(result.value)
+            }
         }
 
-        return ok(f(result.value), result.matched, result.remaining)
+        return ok(values, input, remaining)
+    }
+
+    public static tokens(...parsers: Parser[]): Parser {
+        return new Sequence(intersperse(parsers, spc))
     }
 }
 
@@ -114,49 +145,48 @@ function intersperse(list: any[], sep: any): any[] {
     return list.flatMap(item => [sep, item]).slice(1)
 }
 
-function expression(input: string): ParseResult {
-    //const newline = alternative(["\n", "\r", "\r\n"].map(str))
+class Expression extends Parser {
+    parse(input: string): ParseResult {
+        const expression = this
+        //const newline = alternative(["\n", "\r", "\r\n"].map(str))
 
-    const number = withValue(
-        repeat(alternative("0123456789".split("").map(str))),
-        (value: string) => parseInt(value)
-    )
+        const number = new Repeat(
+            new Alternative("0123456789".split("").map(n => new StringParser(n)))
+        ).withValue(parseInt)
 
-    const kw = {
-        any: str("any", EXP.any()),
-        maybe: str("maybe"),
-        many: str("many"),
-        of: str("of"),
-        to: str("to"),
-    }
-
-    const any = kw.any
-    const maybe = withValue(tokenSequence(kw.maybe, expression), (value: EXP.Expression[]) =>
-        EXP.maybe(value[1])
-    )
-
-    const manyOf = withValue(
-        tokenSequence(kw.many, kw.of, expression),
-        (value: EXP.Expression[]) => {
-            return EXP.manyOf(value[2])
+        const kw = {
+            any: new StringParser("any", EXP.any()),
+            maybe: new StringParser("maybe"),
+            many: new StringParser("many"),
+            of: new StringParser("of"),
+            to: new StringParser("to"),
         }
-    )
-    const countOf = withValue(tokenSequence(number, kw.of, expression), (value: any[]) =>
-        EXP.countOf(value[0], value[2])
-    )
-    const countRangeOf = withValue(
-        tokenSequence(number, kw.to, number, kw.of, expression),
-        (value: any[]) => EXP.countRangeOf(value[0], value[2], value[4])
-    )
 
-    const expressions = [any, maybe, manyOf, countOf, countRangeOf]
+        const any = kw.any
+        const maybe = Sequence.tokens(kw.maybe, expression).withValue((value: EXP.Expression[]) =>
+            EXP.maybe(value[1])
+        )
 
-    return alternative(expressions)(input)
+        const manyOf = Sequence.tokens(kw.many, kw.of, expression).withValue(
+            (value: EXP.Expression[]) => EXP.manyOf(value[2])
+        )
+
+        const countOf = Sequence.tokens(number, kw.of, expression).withValue((value: any[]) =>
+            EXP.countOf(value[0], value[2])
+        )
+
+        const countRangeOf = Sequence.tokens(number, kw.to, number, kw.of, expression).withValue(
+            (value: any[]) => EXP.countRangeOf(value[0], value[2], value[4])
+        )
+
+        const expressions = [any, maybe, manyOf, countOf, countRangeOf]
+
+        return new Alternative(expressions).parse(input)
+    }
 }
 
 export function parse(input: string) {
-    const parseDSL = repeat(expression)
-    const result = parseDSL(input)
+    const result = new Repeat(new Expression()).parse(input)
 
     if (result.value.length === 1) {
         return result.value[0]
