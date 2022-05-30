2d34fe83129810352233ea62814a781952adec96
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-05-30 12:15:21 +0200

    feat: rough version of parser combinator lib

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import * as dsl from "./dsl"
 import {
-    any,
-    countOf,
-    countRangeOf,
-    maybe,
-    manyOf,
-    sequence,
     Expression,
     ExpressionType,
     CountOf,
@@ -15,38 +9,7 @@ import {
 } from "./expression"
 
 export * from "./expression"
-
-const actions = {
-    single_value(_input: any, _start: any, _end: any, elements: any) {
-        return elements[0]
-    },
-    any(_input: any, _start: any, _end: any, _elements: any) {
-        return any()
-    },
-    count_of(_input: any, _start: any, _end: any, elements: any) {
-        return countOf(parseInt(elements[0].text), elements[1])
-    },
-    count_range_of(_input: any, _start: any, _end: any, elements: any) {
-        return countRangeOf(parseInt(elements[0].text), parseInt(elements[1].text), elements[2])
-    },
-    many_of(_input: any, _start: any, _end: any, elements: any) {
-        return manyOf(elements[0])
-    },
-    maybe(_input: any, _start: any, _end: any, elements: any) {
-        return maybe(elements[0])
-    },
-    sequence(_input: any, _start: any, _end: any, [head, tail]: any): any {
-        if (tail.type === ExpressionType.SEQUENCE) {
-            return sequence([head, ...tail.value])
-        } else {
-            return sequence([head, tail])
-        }
-    },
-}
-
-export function parse(input: string) {
-    return dsl.parse(input, { actions })
-}
+export * from "./parsing"
 
 export function compile(ast: Expression): string {
     if (
---
+++ src/parsing.ts
@@ -0,0 +1,115 @@
+import { any as anyExp } from "./expression"
+
+interface ParseResult {
+    isSuccess: boolean
+    remaining: string
+    matched: string
+    value: any
+}
+
+type Parser = (input: string) => ParseResult
+
+function ok(value: any, matched: string, remaining: string): ParseResult {
+    return { isSuccess: true, value, matched, remaining }
+}
+
+function err(input: string, message: string): ParseResult {
+    return { isSuccess: false, value: message, matched: "", remaining: input }
+}
+
+function str(str: string, value?: any): Parser {
+    if (!value) {
+        value = str
+    }
+
+    return function (input: string): ParseResult {
+        if (input.startsWith(str)) {
+            return ok(value, str, input.slice(str.length))
+        } else {
+            return err(input, `expected '${str}' but got '${input}'`)
+        }
+    }
+}
+
+// TODO format value
+function sequence(...parsers: Parser[]): Parser {
+    return function (input: string): ParseResult {
+        let values: any[] = []
+        let remaining = input
+        for (const p of parsers) {
+            const result = p(remaining)
+
+            if (!result.isSuccess) {
+                return result // is the error
+            }
+
+            remaining = result.remaining
+            values.push(result.value)
+        }
+
+        return ok(values, input, remaining)
+    }
+}
+
+function alternative(...parsers: Parser[]): Parser {
+    return function (input: string): ParseResult {
+        const errors: string[] = []
+        for (const p of parsers) {
+            const result = p(input)
+
+            if (result.isSuccess) {
+                return result
+            }
+
+            errors.push(result.value)
+        }
+        return err(input, `no alternative matched: \n${errors.map(err => "    " + err).join("\n")}`)
+    }
+}
+
+function repeat(parser: Parser, optional: boolean = false): Parser {
+    return function (input: string): ParseResult {
+        let values: any[] = []
+
+        let result = parser(input)
+
+        while (result.isSuccess) {
+            values.push(result.value)
+            result = parser(result.remaining)
+        }
+
+        if (!optional && values.length === 0) {
+            return err(input, `expected at least one but ran into error: ${result.value}`)
+        }
+
+        return ok(values, input.slice(0, result.remaining.length), result.remaining)
+    }
+}
+
+function expression(input: string): ParseResult {
+    const spc = repeat(alternative(str(" "), str("\t")))
+    const newline = alternative(...["\n", "\r", "\r\n"].map(str))
+    const kw = {
+        any: str("any", anyExp()),
+        maybe: str("maybe"),
+        many: str("many"),
+        of: str("of"),
+    }
+
+    const any = kw.any
+    const maybe = sequence(kw.maybe, spc, expression)
+
+    const expressions = [any, maybe]
+
+    return alternative(...expressions)(input)
+}
+
+export function parse(input: string) {
+    const parseDSL = repeat(expression)
+    const result = parseDSL(input)
+
+    console.log(result)
+    return result.value
+}
+
+console.log(parse("maybe any"))
