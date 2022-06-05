0c1bc133b9ed10e7422ffd2bc861ce60833687b9
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-06-05 15:06:10 +0200

    feat(verify): add nodejs regex engine

--- test/verification.test.ts
+++ test/verification.test.ts
@@ -18,7 +18,12 @@ const PERL: RegexEngine = {
         runProcess("perl", ["test/engines/perl/run.pl"], regex, input),
 }
 
-const ENGINES: [string, RegexEngine][] = [PYTHON, PERL].map(e => [e.name, e])
+const NODEJS: RegexEngine = {
+    name: "nodejs",
+    match: (regex: string, input: string) => new RegExp(regex).test(input),
+}
+
+const ENGINES: [string, RegexEngine][] = [PYTHON, PERL, NODEJS].map(e => [e.name, e])
 
 interface TestCase {
     pattern: Expression
@@ -31,10 +36,10 @@ const TEST_CASES: [string, TestCase][] = [
     { pattern: literal("hello"), input: "hello", matches: true, matchStart: 0, matchEnd: 5 },
     { pattern: literal("hello"), input: "bye", matches: false },
 ].map(c => {
-    return [c.pattern.toString(), c]
+    return [`${c.pattern.toString()} ${c.matches ? "positive" : "negative"}`, c]
 })
 
-describe.each(ENGINES)("compiles correctly to %s regex", (_engineName, engine) => {
+describe.each(ENGINES)("%s regex", (_engineName, engine) => {
     test.each(TEST_CASES)("%s", async (_name, { pattern, input, ...expected }) => {
         const regex = compile(pattern)
 
