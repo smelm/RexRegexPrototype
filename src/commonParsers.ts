408333d8cfd96081de3511f71e5d058a40f89658
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-07-04 10:20:43 +0200

    refactor: rename single space and newline parser

--- src/commonParsers.ts
+++ src/commonParsers.ts
@@ -1,11 +1,14 @@
 import { StringParser, AlternativeParser, RepeatParser } from "./parsing"
 
-export const space = new AlternativeParser([new StringParser(" "), new StringParser("\t")]).ignore()
-export const spaces = new RepeatParser(space, false).ignore()
-export const optionalSpaces = new RepeatParser(space, true).ignore()
+export const singleSpace = new AlternativeParser([
+    new StringParser(" "),
+    new StringParser("\t"),
+]).ignore()
+export const spaces = new RepeatParser(singleSpace, false).ignore()
+export const optionalSpaces = new RepeatParser(singleSpace, true).ignore()
 
-export const newline = new AlternativeParser(
+export const singleNewline = new AlternativeParser(
     ["\n", "\r", "\r\n"].map(s => new StringParser(s))
 ).ignore()
-export const newlines = new RepeatParser(newline, false).ignore()
-export const optionalNewlines = new RepeatParser(newline, true).ignore()
+export const newlines = new RepeatParser(singleNewline, false).ignore()
+export const optionalNewlines = new RepeatParser(singleNewline, true).ignore()
