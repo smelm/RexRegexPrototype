24990552a37e90f88e14481ab4a8dbef90c77449
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-11-26 15:20:34 +0100

    feat: match beginning/end depending on settings

--- src/ast/DSLScript.ts
+++ src/ast/DSLScript.ts
@@ -1,14 +1,23 @@
-import { RandomSeed } from "random-seed";
-import { Expression, ExpressionType } from "./Expression";
-import { WrappingExpression } from "./WrappingExpression";
+import { RandomSeed } from "random-seed"
+import { Expression, ExpressionType } from "./Expression"
+import { WrappingExpression } from "./WrappingExpression"
+
+enum PositionInInput {
+    BEGINNING,
+    END,
+    WITHIN,
+    WHOLE,
+}
 
 export class ScriptSettings {
-    constructor(public readonly startOfInput: boolean = false, public readonly endOfInput: boolean = false){}
+    // default to whole string match
+    constructor(public readonly positionInInput: PositionInInput = PositionInInput.WHOLE) {}
 }
 
 export class DSLScript extends WrappingExpression {
-    
-    constructor(public child: Expression, private settings?: ScriptSettings) {
+    private settings: ScriptSettings
+
+    constructor(public child: Expression, settings?: ScriptSettings) {
         super(ExpressionType.SCRIPT)
 
         if (!settings) {
@@ -30,7 +39,23 @@ export class DSLScript extends WrappingExpression {
 
     toRegex(): string {
         const pattern = this.child.toRegex()
-        return `^(:?${pattern})$`
+
+        // this seems counter-intuitive
+        // since the DSL defaults to whole string match
+        let [prefix, suffix] = (() => {
+            switch (this.settings.positionInInput) {
+                case PositionInInput.BEGINNING:
+                    return ["^", ""]
+                case PositionInInput.END:
+                    return ["", "$"]
+                case PositionInInput.WITHIN:
+                    return ["", ""]
+                case PositionInInput.WHOLE:
+                default:
+                    return ["^", "$"]
+            }
+        })()
+
+        return `${prefix}(:?${pattern})$${suffix}`
     }
-   
 }
