ec84ac42114406ca13c16a1e84716f4209b08a3a
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2022-12-25 15:20:25 +0100

    refactor: extract CharRange to own file

--- src/ast/CharacterClass.ts
+++ src/ast/CharacterClass.ts
@@ -2,55 +2,7 @@ import { RandomSeed } from "random-seed"
 import { cummulativeSum, randomInt } from "../utils"
 import { ExpressionType } from "./Expression"
 import { WrappingExpression } from "./WrappingExpression"
-
-class CharRange {
-    constructor(public lower: number, public upper: number) {
-        if (lower > upper) {
-            throw new Error(
-                `invalid range (${lower} - ${upper}): upper bound smaller than lower bound`
-            )
-        }
-    }
-
-    static fromStrings(lower: string, upper: string): CharRange {
-        if (lower.length !== 1 || upper.length !== 1) {
-            throw new Error(`expected single character got ${lower} - ${upper}`)
-        }
-
-        const lowerCode = lower.charCodeAt(0)
-        const upperCode = upper.charCodeAt(0)
-
-        return new CharRange(lowerCode, upperCode)
-    }
-
-    char(index: number): string {
-        if (this.lower + index > this.upper) {
-            throw new Error(
-                `Character index ${index} out of range in char range ${String.fromCharCode(
-                    this.lower
-                )} - ${String.fromCharCode(this.upper)}`
-            )
-        }
-
-        return String.fromCharCode(this.lower + index)
-    }
-
-    toList(): [number, number] {
-        return [this.lower, this.upper]
-    }
-
-    length() {
-        return this.upper - this.lower + 1
-    }
-
-    toString(): string {
-        if (this.lower === this.upper) {
-            return String.fromCharCode(this.lower)
-        } else {
-            return `${String.fromCharCode(this.lower)}-${String.fromCharCode(this.upper)}`
-        }
-    }
-}
+import { CharRange } from "./CharRange"
 
 class RangeList {
     /*
