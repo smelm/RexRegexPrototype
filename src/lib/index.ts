db44dac240ad1346c8f948d90e3972ae4be294b2
Samuel Melm <samuel.melm@stud.uni-heidelberg.de>
2023-01-05 04:36:37 +0100

    feat(numberBetween): add number between

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { Expression, ExpressionType } from "../ast/Expression"
-import { any, literal, manyOf, sequence } from "../ast/astBuilders"
+import { alternative, any, anyOf, literal, manyOf, repeat, sequence } from "../ast/astBuilders"
 import { RandomSeed } from "random-seed"
 
 class NegativeLookAhead extends Expression {
@@ -37,4 +37,73 @@ export function surroundWith(begin: string, end: string = ""): Expression {
         manyOf(sequence(new NegativeLookAhead(literal(end)), any())),
         literal(end)
     )
+}
+
+export function numberBetween(
+    lower: number,
+    upper: number,
+    { leadingZeroes = false }: { leadingZeroes?: boolean } = {}
+): Expression {
+    if (lower > upper) {
+        throw new Error(`lower ${lower} is bigger than upper ${upper}`)
+    }
+
+    let upperDigits = String(upper)
+        .split("")
+        .map(d => Number.parseInt(d))
+    let lowerDigits = String(lower)
+        .split("")
+        .map(d => Number.parseInt(d))
+    // padding
+    lowerDigits = [...new Array(upperDigits.length - lowerDigits.length).fill(0), ...lowerDigits]
+
+    let lowerPrefix = ""
+    let upperPrefix = ""
+    const alternatives = []
+
+    for (let [l, u] of zip(lowerDigits, upperDigits)) {
+        const paddingLength = upperDigits.length - upperPrefix.length - 1
+        const padding =
+            paddingLength > 0 ? [repeat(anyOf(["0", "9"]), paddingLength, paddingLength)] : []
+
+        if (lowerPrefix === upperPrefix) {
+            if (u - l >= 2) {
+                alternatives.push(
+                    sequence(
+                        ...(upperPrefix.length > 0 ? [literal(upperPrefix)] : []),
+                        anyOf([(l + 1).toString(), (u - 1).toString()]),
+                        ...padding
+                    )
+                )
+            }
+        } else {
+            // lower
+            if (l < 9) {
+                alternatives.push(
+                    sequence(literal(lowerPrefix), anyOf([(l + 1).toString(), "9"]), ...padding)
+                )
+            }
+
+            // upper
+            if (u > 0) {
+                alternatives.push(
+                    sequence(literal(upperPrefix), anyOf(["0", (u - 1).toString()]), ...padding)
+                )
+            }
+        }
+
+        if (l !== 0 || leadingZeroes) {
+            lowerPrefix += l.toString()
+        }
+        upperPrefix += u.toString()
+    }
+    return alternative(...alternatives)
+}
+
+export function zip(...arrays: any): any {
+    return Array.apply(null, Array(arrays[0].length)).map(function (_, i) {
+        return arrays.map(function (array: any) {
+            return array[i]
+        })
+    })
 }
